48c9c5c925f4b33323807a74d9b68c8c3267b752
Amel <amel.mch99@gmail.com>
2019-11-29 12:56:58 +0100

    Well

---
+++ src/sample/LProfil.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LProfil {
+    public void GoTraining(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent formation = FXMLLoader.load(getClass().getResource("Formation.fxml"));
+        Scene scene4 = new Scene(formation);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene4);
+        window.show();
+    }
+    public void GoLogin(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent signup = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene2 = new Scene(signup);
+        Stage window = (Stage)((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(scene2);
+        window.show();
+    }
+
+    public void handleClose(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
+}
---
+++ src/sample/Login.java
@@ -0,0 +1,30 @@
+package sample;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.Loader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Login extends Application {
+        @Override
+        public void start(Stage stage) throws Exception
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root, 715, 400));
+            stage.setX(350);
+            stage.setY(150);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+
+}
+
+
